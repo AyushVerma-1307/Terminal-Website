2b77a5e7d7246b235ea43ef8a122ac7f6e7f0d0d
Ayush Verma <ayushsoni2323@gmail.com>
2024-04-17 23:36:12 +0530

    update index.js

--- js/index.js
+++ js/index.js
@@ -13,3 +13,13 @@ function init() {
     cursor.style.left = "0px";
 }
 
+function typeIt(from, e) { // used to update the content of an HTML element, based on the input provided by the user in a text area.
+    e = e || window.event; 
+    // window.event: the current event that is being handled by the browser
+    // e: the event that was triggered by the user action (such as a key press)
+    var w = $("typer");
+    var tw = from.value;
+    if (!pw){
+      w.innerHTML = nl2br(tw);
+    }
+  }
