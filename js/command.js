70477e245274b6982884fb4e2aecc893b5bcd6c9
Ayush Verma <ayushsoni2323@gmail.com>
2024-04-18 20:11:53 +0530

    help command added

--- js/command.js
+++ js/command.js
@@ -1,16 +1,35 @@
-var Portfolio = "https://ayush1307-portfolio.vercel.app/"
-var StudyNotion = "https://study-notion-edtech-website.vercel.app/"
-var YouTwit_Backend = "https://github.com/AyushVerma-1307/YouTwit-Backend"
-var DevFinder = "https://dev-finder-2024.vercel.app/"
-var Weather_App = "https://ayushverma-1307.github.io/Weather-App/"
-var TIC_TAC_TOE = "https://ayushverma-1307.github.io/TIC-TAC-TOE/"
-var TODO_APP = "https://mern-todo-app-pro.vercel.app/"
-var KeyCraft = "https://ayushverma-1307.github.io/KeyCraft/"
-var Virtual_Mouse = "https://github.com/AyushVerma-1307/Virtual-Mouse"
-var Python_ChatBot = "https://github.com/AyushVerma-1307/PyChat"
-var TwinPortal = "https://github.com/AyushVerma-1307/TwinPortal"
-var Roll_the_Dice = "https://github.com/AyushVerma-1307/Roll_the_Dice"
+var Portfolio = "https://ayush1307-portfolio.vercel.app/";
+var StudyNotion = "https://study-notion-edtech-website.vercel.app/";
+var YouTwit_Backend = "https://github.com/AyushVerma-1307/YouTwit-Backend";
+var DevFinder = "https://dev-finder-2024.vercel.app/";
+var Weather_App = "https://ayushverma-1307.github.io/Weather-App/";
+var TIC_TAC_TOE = "https://ayushverma-1307.github.io/TIC-TAC-TOE/";
+var TODO_APP = "https://mern-todo-app-pro.vercel.app/";
+var KeyCraft = "https://ayushverma-1307.github.io/KeyCraft/";
+var Virtual_Mouse = "https://github.com/AyushVerma-1307/Virtual-Mouse";
+var Python_ChatBot = "https://github.com/AyushVerma-1307/PyChat";
+var TwinPortal = "https://github.com/AyushVerma-1307/TwinPortal";
+var Roll_the_Dice = "https://github.com/AyushVerma-1307/Roll_the_Dice";
 
-var email ="mailto:ayushsoni2323@gmail.com"
-var linkedin = "https://www.linkedin.com/in/ayush-verma1307/"
-var github = "https://github.com/AyushVerma-1307"
+var email = "mailto:ayushsoni2323@gmail.com";
+var linkedin = "https://www.linkedin.com/in/ayush-verma1307/";
+var github = "https://github.com/AyushVerma-1307";
+
+help = [
+    "+========================================================================================+",
+    "| Commands   | Descriptions                                                              |",
+    "+========================================================================================+",
+    "| whois      | Who is Ayush Verma?                                                       |",
+    "| ayush      | Discover more about me                                                    |",
+    "| projects   | View Coding Projects                                                      |",
+    "| social     | Display Social Networks                                                   |",
+    "| history    | View command history                                                      |",
+    "| help       | Ah, yes. You're a command whisperer! You already know what this one does. |",
+    "| email      | Please don't spam my email                                                |",
+    "| clear      | Clear terminal                                                            |",
+    "| banner     | Display the header                                                        |",
+    "| linkedin   | Checkout my LinkedIn profile for the latest updates!                      |",
+    "| gitHub     | Discover more projects on my GitHub profile                               |",
+    "| codevoke   | Unveil the obscurity that lies within me.                                 |",
+    "+========================================================================================+",
+];
